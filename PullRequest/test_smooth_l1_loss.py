c730d19746fb8f5c9e1b9db531e7eb3871ca1dfd
LuisMelendez <luiguimelendez5@gmail.com>
2025-12-15 09:47:45 -0600

    Create test_smooth_l1_loss.py

---
+++ PullRequest/test_smooth_l1_loss.py
@@ -0,0 +1,25 @@
+import torch
+
+from pytorch_layer_test_class import PytorchLayerTest
+
+
+class TestSmoothL1Loss(PytorchLayerTest):
+
+    def _prepare_input(self):
+        return {
+            "x": torch.randn(2, 3),
+            "y": torch.randn(2, 3)
+        }
+
+    def create_model(self):
+        class SmoothL1LossModel(torch.nn.Module):
+            def forward(self, x, y):
+                return torch.nn.functional.smooth_l1_loss(x, y)
+
+        return SmoothL1LossModel(), None, "aten::smooth_l1_loss"
+
+    def test_smooth_l1_loss(self, ie_device, precision):
+        self._test(
+            ie_device=ie_device,
+            precision=precision
+        )
